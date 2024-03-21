89e6b1c2ec3793a191112729c6e50a6833634378
Riantsoa11 <tsiory.andrianasoloharison@gmail.com>
2024-03-21 19:51:25 +0100

    Tsiory

--- Agenda_V1_mety/Agenda_V1_mety/View/ContactPage.xaml.cs
+++ Agenda_V1_mety/Agenda_V1_mety/View/ContactPage.xaml.cs
@@ -18,9 +18,7 @@ using Agenda_V1_mety.Agenda_tsiory;
 
 namespace Agenda_V1_mety.View
 {
-    /// <summary>
-    /// Logique d'interaction pour Contact.xaml
-    /// </summary>
+    
     public partial class ContacPage : UserControl
     {
         DAO_cantact dAO_Contact;
@@ -40,7 +38,6 @@ namespace Agenda_V1_mety.View
 
         private void BTN_Ajouter_Click(object sender, RoutedEventArgs e)
         {
-            //ajouter un contact quand je clique sur le bouton ajouter
             AjouterPage ajouterPage = new AjouterPage();
             this.Content = ajouterPage;
 
@@ -55,7 +52,6 @@ namespace Agenda_V1_mety.View
 
         private void BTN_Modifier_Click(object sender, RoutedEventArgs e)
         {
-            //je clique sur la ligne que je veux modifier et je change ce que je veux et je clique sur le bouton modifier
             Contact contact = DG_Contact.SelectedItem as Contact;
             dAO_Contact.modifieContact(contact);
             DG_Contact.ItemsSource = dAO_Contact.GetContacts();
@@ -64,27 +60,22 @@ namespace Agenda_V1_mety.View
 
         private void BTN_Famille_Click(object sender, RoutedEventArgs e)
         {
-            //je clique sur le bouton famille pour afficher les contacts de la famille
             DG_Contact.ItemsSource = dAO_Contact.GetContacts().Where(c => c.Statut == "Famille").ToList();
         }
 
         private void BTN_Amis_Click(object sender, RoutedEventArgs e)
         {
-            //je clique sur le bouton amis pour afficher les contacts des amis
             DG_Contact.ItemsSource = dAO_Contact.GetContacts().Where(c => c.Statut == "Amis").ToList();
         }
 
         private void BTN_College_Click(object sender, RoutedEventArgs e)
         {
-            //je clique sur le bouton college pour afficher les contacts des collegues
             DG_Contact.ItemsSource = dAO_Contact.GetContacts().Where(c => c.Statut == "Collegue").ToList();
         }
 
         private void BTN_Recherche_Click(object sender, RoutedEventArgs e)
         {
-            //je recherche un contact par son nom
             DG_Contact.ItemsSource = dAO_Contact.RechercherContactParNom(TB_Recherche.Text);
-            //je recherche un contact par son prenom
             DG_Contact.ItemsSource = dAO_Contact.RechercherContactParPrenom(TB_Recherche.Text);
         }
         
